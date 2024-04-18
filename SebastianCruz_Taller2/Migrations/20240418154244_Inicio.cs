19aacccbe7f08ea84a6c43f77c12a35d0ceb7291
SEBASTIAN CRUZ <sebascruzb03@hotmail.com>
2024-04-18 11:59:05 -0500

    Terminado

---
+++ SebastianCruz_Taller2/Migrations/20240418154244_Inicio.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SebastianCruz_Taller2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Burger",
+                columns: table => new
+                {
+                    BurgerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WithCheese = table.Column<bool>(type: "bit", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Burger", x => x.BurgerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Promo",
+                columns: table => new
+                {
+                    PromoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaPromo = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BurguerID = table.Column<int>(type: "int", nullable: false),
+                    BurgerID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promo", x => x.PromoID);
+                    table.ForeignKey(
+                        name: "FK_Promo_Burger_BurgerID",
+                        column: x => x.BurgerID,
+                        principalTable: "Burger",
+                        principalColumn: "BurgerID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Promo_BurgerID",
+                table: "Promo",
+                column: "BurgerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Promo");
+
+            migrationBuilder.DropTable(
+                name: "Burger");
+        }
+    }
+}
